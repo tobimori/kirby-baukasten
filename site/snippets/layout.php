b1fe850a8cc90bc36f7f594a6518d5dbed768c35
tobimori <tobias@moeritz.cc>
2023-03-20 12:33:57 +0100

    update layout snippet

--- site/snippets/layout.php
+++ site/snippets/layout.php
@@ -1,13 +1,17 @@
 <?php
 
-/** @var Kirby\Cms\Page $page */
+/** @var Kirby\Cms\Page $page
+ *  @var Kirby\Cms\Site $site
+ *  @var Kirby\Cms\App $kirby */
 
-if (json_decode(env('REQUIRES_LOGIN')) && !kirby()->user()) {
+use Kirby\Filesystem\F;
+
+if (json_decode(env('REQUIRES_LOGIN')) && !$kirby->user()) {
   go('/panel');
 } ?>
 
 <!DOCTYPE html>
-<html lang="de">
+<html lang="<?= $kirby->language()->code() ?>">
 
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -34,7 +38,7 @@ if (json_decode(env('REQUIRES_LOGIN')) && !kirby()->user()) {
     <?= $slot ?>
   </div>
   <script>
-    console.log('%ccoralic/kirby-baukasten<?= file_exists($kirby->roots()->base() . '/.git/refs/heads/main') ? '#' . substr(file_get_contents($kirby->roots()->base() . '/.git/refs/heads/main'), 0, 7) : '' ?>', 'font-size: 12px; font-weight: bold; color: #F9E7FF; background-color: #7D3BA8; padding: 10px 16px; margin: 20px 0; border-radius: 4px;')
+    console.log('%ccoralic/kirby-baukasten<?= F::exists($kirby->roots()->base() . '/.git/refs/heads/main') ? '#' . substr(file_get_contents($kirby->roots()->base() . '/.git/refs/heads/main'), 0, 7) : '' ?>', 'font-size: 12px; font-weight: bold; color: #F9E7FF; background-color: #7D3BA8; padding: 10px 16px; margin: 20px 0; border-radius: 4px;')
     console.log('%cMade with ♥ in Germany by coralic', 'font-size: 12px; font-weight: bold; color: #0e1f28; background-color: #fff; padding: 10px 16px; margin: 20px 0; border-radius: 4px;')
   </script>
 </body>
