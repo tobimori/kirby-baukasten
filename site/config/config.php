3f163145172d89d86a31ceceb93b233f279f71cb
Tobias Möritz <tobias@moeritz.cc>
2023-10-12 23:04:31 +0200

    add missing image presets

--- site/config/config.php
+++ site/config/config.php
@@ -15,6 +15,14 @@ return [
 	'yaml.handler' => 'symfony',
 	'date.handler' => 'intl',
 	'languages' => true,
+	/** Image presets */
+	'thumbs' => [
+		'srcsets' => [
+			'small' => [300, 500, 800],
+			'default' => [400, 800, 1000, 1200],
+			'large' => [800, 1400, 2000, 2600],
+		]
+	],
 	/** SEO */
 	'tobimori.seo' => [
 		'robots' => [
