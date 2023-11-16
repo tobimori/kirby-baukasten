271aee6b770d8384d4b810ba7e44b3bfd3e4e19c
tobimori <tobias@moeritz.cc>
2023-11-16 17:23:26 +0100

    Update to Kirby 4.0-rc.1

--- public/index.php
+++ public/index.php
@@ -2,19 +2,6 @@
 
 use Kirby\Cms\App;
 
-/**
- * Smart version of echo with an if condition as first argument
- *
- * @param mixed $value The string to be echoed if the condition is true
- * @param mixed $alternative An alternative string which should be echoed when the condition is false
- */
-function e(mixed $condition, mixed $value, mixed $alternative = null): void
-{
-	echo $condition ? $value : $alternative;
-}
-
-define('KIRBY_HELPER_E', false);
-define('KIRBY_HELPER_ATTR', false);
 define("KIRBY_HELPER_DUMP", false);
 
 $base = dirname(__DIR__);
