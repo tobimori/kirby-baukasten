109de19ddae1279de13cd7af28221eb22f87a022
tobimori <tobias@moeritz.io>
2024-04-10 18:25:24 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,34 @@
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import laravel from 'laravel-vite-plugin'
-import 'dotenv/config'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : '/dist/',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
 
-  build: {
-    outDir: resolve(__dirname, 'public/dist'),
-    emptyOutDir: true,
-    manifest: 'manifest.json'
-  },
-  plugins: [
-    laravel({
-      input: ['src/index.ts', 'src/styles/index.css', 'src/styles/panel.css'],
-      refresh: ['site/{layouts,snippets,templates}/**/*'],
-      transformOnServe: (code) =>
-        code.replaceAll(
-          `/assets`,
-          `http://${process.env.KIRBY_DEV_HOSTNAME ?? '127.0.0.1'}:${
-            process.env.KIRBY_DEV_PORT ?? '3000'
-          }/assets`
-        )
-    })
-  ],
-  resolve: {
-    alias: {
-      '@styles': resolve(__dirname, 'src/styles/'),
-      '@': resolve(__dirname, 'src/')
+  return {
+    base: mode === 'development' ? '/' : '/dist/',
+
+    build: {
+      outDir: resolve(__dirname, 'public/dist'),
+      emptyOutDir: true,
+      manifest: 'manifest.json'
+    },
+    plugins: [
+      laravel({
+        input: ['src/index.ts', 'src/styles/index.css', 'src/styles/panel.css'],
+        refresh: ['site/{layouts,snippets,templates}/**/*'],
+        transformOnServe: (code) =>
+          code.replaceAll(
+            `/assets`,
+            `http://${env.KIRBY_DEV_HOSTNAME ?? '127.0.0.1'}:${env.KIRBY_DEV_PORT ?? '3000'}/assets`
+          )
+      })
+    ],
+    resolve: {
+      alias: {
+        '@styles': resolve(__dirname, 'src/styles/'),
+        '@': resolve(__dirname, 'src/')
+      }
     }
   }
-}))
+})
