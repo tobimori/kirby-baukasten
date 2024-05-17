e1338e90a8cd0bf415da4e8163d27682c9cbe23b
tobimori <tobias@moeritz.io>
2024-05-17 12:14:56 +0200

    sprite should not be commited

--- src/controllers/hello-world.ts
+++ src/controllers/hello-world.ts
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
+import { Typed } from "stimulus-typescript"
 
-export default class extends Controller {
+export default class extends Typed(Controller, {}) {
 	connect() {
 		console.log("Hello world!")
 	}
